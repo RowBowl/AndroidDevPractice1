017e085077d81d78eca8483e75e23f29aa6d03d2
RowBowl <robelrb@gmail.com>
2023-09-05 14:23:04 -0400

    extracting string resources

--- app/src/main/java/com/example/androiddevpractice1/MainActivity.kt
+++ app/src/main/java/com/example/androiddevpractice1/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,7 +17,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -33,7 +37,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    HelloWorldImage(message = "Hello World!", from = "From Robel")
+                    HelloWorldImage(message = stringResource(R.string.hello_world_text),
+                        from = stringResource(R.string.hello_world_from))
                 }
             }
         }
@@ -68,7 +73,9 @@ fun HelloWorldImage(message: String, from: String, modifier: Modifier = Modifier
     Box {
         Image(
             painter = image,
-            contentDescription = null //because this is a simple background image
+            contentDescription = null, //because this is a simple background image
+            contentScale = ContentScale.Crop, //fill screen
+            alpha = 0.5F
         )
         HelloWorldText(
             message = message,
@@ -84,6 +91,6 @@ fun HelloWorldImage(message: String, from: String, modifier: Modifier = Modifier
 fun GreetingPreview() {
     AndroidDevPractice1Theme {
         //HelloWorldText(message = "Hello World!", from = "From Robel")
-        HelloWorldImage(message = "Hello World!", from = "From Robel")
+        HelloWorldImage(message = stringResource(R.string.hello_world_text), from = stringResource(R.string.hello_world_from))
     }
 }
